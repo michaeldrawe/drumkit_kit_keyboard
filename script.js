92181dc45551feee0cfdbed3c40f5e27cf88b728
Michael Drawe <michaeldrawe@gmail.com>
2020-01-22 23:47:59 -0700

    updated mobile page

--- script.js
+++ script.js
@@ -18,63 +18,63 @@ function removeTransition(e) {
   window.addEventListener('keydown', playSound);
 
   function soundClap() {
-    console.log("function soundClap has been called");
+    
     var path = "sounds/clap.wav";
     var snd = new Audio (path);
     snd.play();
   }
 
   function soundHihat() {
-    console.log("function soundHihat has been called");
+    
     var path = "sounds/hihat.wav";
     var snd = new Audio (path);
     snd.play();
   }
 
   function soundKick() {
-    console.log("function soundKick has been called")
+   
     var path = "sounds/kick.wav"
     var snd = new Audio(path)
     snd.play()
   }
 
   function soundOpenhat() {
-    console.log("function soundOpenhat has been called")
+    
     var path = "sounds/openhat.wav"
     var snd = new Audio(path)
     snd.play()
   }
 
   function soundBoom() {
-    console.log("function soundBoom has been called")
+    
     var path = "sounds/boom.wav"
     var snd = new Audio(path)
     snd.play()
   }
 
   function soundRide() {
-    console.log("function soundRide has been called")
+    
     var path = "sounds/ride.wav"
     var snd = new Audio(path)
     snd.play()
   }
 
   function soundSnare() {
-    console.log("function soundSnare has been called")
+    
     var path = "sounds/snare.wav"
     var snd = new Audio(path)
     snd.play()
   }
 
   function soundTom() {
-    console.log("function soundTom has been called")
+    
     var path = "sounds/tom.wav"
     var snd = new Audio(path)
     snd.play()
   }
 
   function soundTink() {
-    console.log("function soundTink has been called")
+   
     var path = "sounds/tink.wav"
     var snd = new Audio(path)
     snd.play()
